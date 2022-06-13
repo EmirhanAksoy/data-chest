20a01b325016ca352900201b123403d5bec2d129
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2022-06-13 23:54:36 +0300

    Cache configurations added on Program.cs

--- data-chest-service/data-chest-service/Program.cs
+++ data-chest-service/data-chest-service/Program.cs
@@ -1,5 +1,7 @@
+using Alachisoft.NCache.Web.SessionState;
 using email_service.Configuration;
 using Serilog;
+using StackExchange.Redis;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -27,6 +29,54 @@ builder.Services.AddSwaggerGen();
 // Email service configuration
 builder.Services.Configure<EmailConfiguration>(builder.Configuration?.GetSection(nameof(EmailConfiguration)));
 builder.Services.AddSingleton<EmailConfiguration>();
+
+// Cache configurations
+string? cacheOption = builder.Configuration?.GetSection("Cache:Value")?.Value;
+switch (cacheOption)
+{
+    case "RedisCache":
+        {
+            string? endPoint = builder.Configuration?.GetSection("Cache:RedisCache:Endpoint")?.Value;
+            string? portValue = builder.Configuration?.GetSection("Cache:RedisCache:Port")?.Value;
+            int port = int.TryParse(portValue, out var portVal) ? portVal : 8080;
+
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.ConfigurationOptions = new ConfigurationOptions
+                {
+                    EndPoints = { { endPoint, port } },
+                    User = builder.Configuration?.GetSection("Cache:RedisCache:Username")?.Value,
+                    Password = builder.Configuration?.GetSection("Cache:RedisCache:Password")?.Value
+                };
+                options.InstanceName = string.Empty;
+            });
+
+            break;
+        }
+    case "NCache":
+        {
+            builder.Services.AddNCacheSession(builder.Configuration?.GetSection("Cache:NCache"));
+            break;
+        }
+    case "MemoryCache":
+        {
+            builder.Services.AddDistributedMemoryCache();
+            break;
+        }
+    case "SqlServerCache":
+        {
+            builder.Services.AddDistributedSqlServerCache(options =>
+            {
+                options.ConnectionString = builder.Configuration.GetConnectionString("DBConnectionString");
+                options.SchemaName = builder.Configuration?.GetSection("Cache:SqlServerCache:Schema").Value;
+                options.TableName = builder.Configuration?.GetSection("Cache:SqlServerCache:Table").Value;
+            });
+            break;
+        }
+    default:
+        break;
+}
+
 
 var app = builder.Build();
 
