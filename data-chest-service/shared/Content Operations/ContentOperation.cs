f9708d3e7d6ba3cc4826cce5434585c1ea03a539
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2022-06-18 00:58:56 +0300

    Content operations structure was added.

---
+++ data-chest-service/shared/Content Operations/ContentOperation.cs
@@ -0,0 +1,17 @@
+﻿using domain.Models;
+
+namespace shared.Content_Operations
+{
+    public class ContentOperation : IContentOperation
+    {
+        public Task CreateContent(Content content,string parentPath = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteContent(string path,bool includeSubItems = true)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ data-chest-service/shared/Content Operations/IContentOperation.cs
@@ -0,0 +1,12 @@
+﻿using domain.Models;
+
+namespace shared.Content_Operations
+{
+    public interface IContentOperation
+    {
+        Task CreateContent(Content content, string parentPath = "");
+
+        Task DeleteContent(string path,bool includeSubItems = true);
+
+    }
+}
